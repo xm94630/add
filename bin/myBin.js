4ec45054caa708d88247ce0b481f35109f8f8f27
xm94630 <xm94630@126.com>
2017-11-12 09:52:11 +0800

    修改bin

--- bin/myBin.js
+++ bin/myBin.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
-
 var add = require('../index.js');
+
+var a = process.argv[2];
+var b = process.argv[3];
+if (!a || !b)
+  throw new Error('需要两个参数');
+
+add(a,b)
+
 
 console.log('哟哟，我就是全局命令。但是什么也没有做~');
 console.log(add.toString());
