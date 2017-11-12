e61fa06b25ee96a666358fce41faa6fdc1ada823
xm94630 <xm94630@126.com>
2017-11-12 10:07:08 +0800

    实现一个完整的node 命令行的相加功能

--- bin/myBin.js
+++ bin/myBin.js
@@ -1,13 +1,10 @@
 #!/usr/bin/env node
 var add = require('../index.js');
 
-var a = process.argv[2];
-var b = process.argv[3];
+var a = parseFloat(process.argv[2]);
+var b = parseFloat(process.argv[3]);
 if (!a || !b)
   throw new Error('需要两个参数');
 
-add(a,b)
-
-
-console.log('哟哟，我就是全局命令。但是什么也没有做~');
-console.log(add.toString());
+console.log(a+'、'+b+'相加的结果是:');
+console.log(add(a,b));
